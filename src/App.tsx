fe8439d5974d600ee3bebdca80064a94082c9a8e
Anthony Ashton <anthony+git@ashton.one>
2021-12-09 20:10:59 +1100

    :sparkles: Add in bg-color matching from photo

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,11 @@
-import React from 'react';
 import './App.css';
 import { ImageViewer } from './components/image-viewer';
 
 function App() {
 	return (
-		<div className="App">
-			{/* <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a> */}
-			<ImageViewer />
-		</div>
+		// <div className="App">
+		<ImageViewer />
+		// </div>
 	);
 }
 
--- src/assets/index.tsx
+++ src/assets/index.tsx
@@ -1,11 +1,9 @@
 import BarryBox from './barry-box.png'
 import Bee from './bee.png'
-import Cute from './cute.png'
 import Tree from './tree.png'
 
 export {
 	BarryBox,
 	Bee,
-	Cute,
 	Tree,
 }
